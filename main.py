f6406efe3e273275bf4b1e9f06dd3a26dfe9b00f
Adrian Malec <67909049+f10k1@users.noreply.github.com>
2023-02-07 22:18:53 +0100

    added simple password input

--- main.py
+++ main.py
@@ -1,14 +1,44 @@
-from pico_i2c_lcd import I2cLcd
-from machine import I2C
 import rp2
-from machine import Pin
+from config import *
+import uasyncio
+from methods import get_input
+import time
+import json
 
 rp2.country('PL')
 
-buttonUp = Pin(28, Pin.IN, Pin.PULL_DOWN)
-buttonOk = Pin(27, Pin.IN, Pin.PULL_DOWN)
-buttonDown = Pin(26, Pin.IN, Pin.PULL_DOWN)
-buttonConfirm = Pin(15, Pin.IN, Pin.PULL_DOWN)
+async def main():
+    try:
+        raise Exception('')
+        file = open('config.txt', 'r')
+        configuration = json.loads(file.read())
+        file.close()
+        if ('password' in configuration):
+            pass
+            #TODO LCD output
+            #TODO password check
+            #TODO internet connection
+        else:
+            raise Exception("No configuration")
+    except:
+        lcd.putstr("Config not detected")
+        time.sleep(2)
+        lcd.clear()
 
-i2c = I2C(id=0,scl=Pin(17),sda=Pin(16),freq=100000)
-lcd = I2cLcd(i2c, 0x27, 2, 16)
+        lcd.putstr("Enter new password")
+        time.sleep(2)
+        lcd.clear()
+
+        config = {}
+
+        #TODO Secure password by hashing it
+        config['password'] = await get_input()
+        lcd.putstr(json.dumps(config['password']))
+
+        #TODO Input for wifi credentials
+
+        file = open('config.txt', 'w+')
+        file.write(json.dumps(config))
+        file.close()
+
+uasyncio.run(main())
